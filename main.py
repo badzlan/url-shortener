a2f95593d3e364d056a0ddff095399f157aef3fe
badzlan <badzlandhabith05@gmail.com>
2022-12-30 23:53:26 +0700

    final commit

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import pyshorteners as pyst
+import pyperclip
 
 st.markdown("<h1 style='text-align: center;'>URL Shortener</h1>", unsafe_allow_html=True)
 form = st.form("name")
 url =  form.text_input("URL Here")
 s_btn = form.form_submit_button("Short")
 
+def copying():
+    pyperclip.copy(shorted_url)
+
 shortener = pyst.Shortener()
 if s_btn:
     shorted_url = shortener.tinyurl.short(url)
-    print(shorted_url)
+    st.markdown("<h3>Shorted URL</h3>", unsafe_allow_html=True)
+    st.markdown(f"<h6 style='text-align: center;'>{shorted_url}</h6>", unsafe_allow_html=True)
+    st.button("Copy", on_click=copying)
