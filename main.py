65323e6dfff2266e346ba705587f26977b1e2140
badzlan <badzlandhabith05@gmail.com>
2022-12-30 23:34:19 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pyshorteners as pyst
+
+st.markdown("<h1 style='text-align: center;'>URL Shortener</h1>", unsafe_allow_html=True)
+form = st.form("name")
+url=form.text_input("URL Here")
+form.form_submit_button("Short")
+
+shortener = pyst.Shortener()
