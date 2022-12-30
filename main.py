385e004319f4bf12f6eaa5bff04933d7f1d92c91
badzlan <badzlandhabith05@gmail.com>
2022-12-30 23:41:26 +0700

    local url shorted

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import pyshorteners as pyst
 
 st.markdown("<h1 style='text-align: center;'>URL Shortener</h1>", unsafe_allow_html=True)
 form = st.form("name")
-url=form.text_input("URL Here")
-form.form_submit_button("Short")
+url =  form.text_input("URL Here")
+s_btn = form.form_submit_button("Short")
 
 shortener = pyst.Shortener()
+if s_btn:
+    shorted_url = shortener.tinyurl.short(url)
+    print(shorted_url)
